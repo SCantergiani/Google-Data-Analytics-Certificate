9c5c8dd9f36417470eb8f7feecd5b0794dee6090
Sebastian Cantergiani <sebastiancantergiani@gmail.com>
2023-03-01 00:30:57 -0300

    sql update

--- SQL_Documentation.sql
+++ SQL_Documentation.sql
@@ -1,4 +1,4 @@
--- Merge all tables and add columns ride_length and day_of_week --
+-- Merge all tables and add columns ride_length and day_of_week 
 
 SELECT
  DISTINCT *, date_diff(ended_at,started_at,MINUTE) AS ride_length, EXTRACT(DAYOFWEEK FROM started_at) AS  day_of_week
@@ -17,3 +17,39 @@ FROM
   UNION DISTINCT SELECT * FROM `cyclistic_data.202212-divvy-tripdata`)
 
 -- Note: we have 5436715 rows --
+
+*Data validation*
+
+-- Check Duplicates
+
+SELECT
+  DISTINCT COUNT(ride_id)
+FROM
+  `cyclistic_data.2022_tripdata`
+
+
+Row count did not change, that means there are no duplicates.
+
+Check if there is NULL values:
+SELECT
+  COUNT(*)
+FROM
+  `cyclistic_data.2022_tripdata`
+WHERE (
+  ride_id IS NULL
+  OR rideable_type IS NULL
+  OR started_at IS NULL
+  OR ended_at IS NULL
+  OR start_station_name IS NULL
+  OR start_station_id IS NULL
+  OR end_station_name IS NULL
+  OR end_station_id IS NULL
+  OR start_lat IS NULL
+  OR start_lng IS NULL
+  OR end_lat IS NULL
+  OR end_lng IS NULL
+  OR member_casual IS NULL
+  OR ride_length IS NULL
+  OR day_of_week IS NULL)
+
+-- 1067355 rows with at least one NULL cell.
